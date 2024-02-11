ba82870e837493a2bf8d3a933f6759a6ba6cad06
Shashi Prabha <122176467+WednesdaySP@users.noreply.github.com>
2024-02-11 12:27:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        //lecture 1 and 2.
+        //Simple arithmetic operations in Java
+
+        int a=10;
+        int b=5;
+        int ans=(a*b)/(a-b);
+        System.out.println(ans);
+
+        Scanner sc=new  Scanner(System.in);
+        String name =sc.nextLine();
+        System.out.println(name);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int sum= x +y;
+        System.out.println(sum);
+
+        }
+    }
