ff2709ac1025d9a88facce12b58dea74dac7b6de
Shashi Prabha <122176467+WednesdaySP@users.noreply.github.com>
2024-03-04 05:55:17 +0530

    operators in java

---
+++ Operators_BinaryNumSys.java
@@ -0,0 +1,36 @@
+public class Operators_BinaryNumSys {
+
+    public static void main(String[] args){
+
+        //OPERATORS
+        //a,b,1,2 => operands
+        //+,-,/,*,% => operators
+
+        //Arithmetic Operators
+        //Binary: +,-,/,*,%
+        //Unary:works on one operator => ++, --
+        //Relational opr:==,!=,<,>,<=,>=
+        //Logical opr:&&,||,!
+        //a++,++a,a--,--a
+        int a=10;
+        int b=5;
+        b=a++;
+        System.out.println(b);
+        b=++a;
+        System.out.println(b);
+
+        //BINARY NUMBER SYSTEM base2
+//        4 to Binary: divide by base2
+        //100 to decimal:multiply base2 power n
+        //octal base8
+        //memory address is written in hexadecimal with base16
+
+        //Bitwise Operator:&,|,^,~,<<,>>
+        //~:gives compliment
+        //^:XOR
+        //<<: binary left shift
+        //>>:binary right shift
+
+
+    }
+}
